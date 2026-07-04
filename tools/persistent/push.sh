e37cd0f5c241b30793172d7e4148e8e0bf7f2376
K4PUTZ <emaildomat@gmail.com>
2026-07-04 19:38:13 -0300

    Alpha BAKE-08 - 2026-07-04

--- tools/persistent/push.sh
+++ tools/persistent/push.sh
@@ -5,7 +5,7 @@
 ################################################################################
 
 # === EDIT THIS FIELD ===
-TAG="Alpha BAKE-07 - $(date +%Y-%m-%d)"
+TAG="Alpha BAKE-08 - $(date +%Y-%m-%d)"
 # =======================
 
 echo "📦 Pushing to repository..."
